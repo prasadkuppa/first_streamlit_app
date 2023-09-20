b18d9ec116c5d1fa7d71a49cb3f504cff2ff9d35
prasadkuppa <prasadkuppa@gmail.com>
2023-09-20 15:01:04 -0400

    Update streamlit_app.py
    variable update for fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlite.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # put  the output in normalized way
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
