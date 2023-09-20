9d7ae89cc6db1f6579d1ab0081879fc9c63c6d0c
prasadkuppa <prasadkuppa@gmail.com>
2023-09-20 14:57:20 -0400

    Update streamlit_app.py
    minor updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
 
-streamlit.header('Prasad Kuppa')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -27,8 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # put  the output in normalized way
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
